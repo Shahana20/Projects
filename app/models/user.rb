017d478f1af94d574df6e0e239ba557634ddac5a
shahanasedin <Shahana@sedintechnologies.com>
2024-11-13 17:22:01 +0530

    Created user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,118 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  include Discard::Model
+  has_one_attached :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :users_batches
+  has_many :batches, through: :users_batches
+  has_and_belongs_to_many :skills
+  has_and_belongs_to_many :user_roles
+  has_and_belongs_to_many :specialized_skills, join_table: :specialized_user_skills, class_name: 'Skill'
+  has_many :education_details, -> { order(end_year: :desc, created_at: :desc) }, dependent: :destroy
+  has_many :carrer_details, -> { order(end_year: :desc, created_at: :desc) }, dependent: :destroy
+  has_many :project_details, dependent: :destroy
+  has_many :reviews, foreign_key: :user_id
+
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
 
+  PROFILE_PHOTO_CONTENT_TYPES = ['image/png', 'image/jpg', 'image/jpeg'].freeze
+
 
   def self.generate_jwt(user)
     payload = { user_id: user.id, exp: 24.hours.from_now.to_i }
     JWT.encode(payload, Rails.application.secret_key_base)
+  end
+
+  private
+
+  def username_does_not_contain_uppercase
+    if username && username =~ /[A-Z]/
+      errors.add(:username, "cannot contain uppercase characters")
+    end
+  end
+
+  def username_does_not_start_with_number
+    if username && username =~ /^[0-9]/
+      errors.add(:username, "cannot start with a number")
+    end
+  end
+
+  def self.active_mentors
+    select('users.id', 'users.username', 'users.first_name', 'users.last_name', 'users.email', 'users.location', 'users.updated_at', 'carrer_details.designation', 'carrer_details.company')
+      .joins(:carrer_details, :user_roles).where(user_roles: { role: 'Mentor' })
+      .where('carrer_details.id = (SELECT MAX(cd.id) FROM carrer_details cd WHERE cd.user_id = users.id AND cd.start_year <= ? AND cd.end_year >= ?)', Date.today.year, Date.today.year)
+      .order(:id)
+  end
+
+  def self.active_candidates
+    select('users.id', 'users.username', 'users.first_name', 'users.last_name', 'users.email', 'users.location', 'users.updated_at', 'carrer_details.designation')
+      .joins(:carrer_details, :user_roles).where(user_roles: { role: 'Candidate' })
+      .where('carrer_details.id = (SELECT MAX(cd.id) FROM carrer_details cd WHERE cd.user_id = users.id AND cd.start_year >= ? AND cd.end_year <= ?)', 2010, Date.today.year)
+      .order(:id)
+  end
+
+  def correct_profile_image_mime_type
+    return unless profile_image.attached? && !profile_image.content_type.in?(PROFILE_PHOTO_CONTENT_TYPES)
+
+    errors.add(:profile_image, 'must be a PNG, JPG, or JPEG file')
+  end
+
+  def user_role_ids
+    user_roles.pluck(:id)
+  end
+
+  def specialized_skill_names
+    specialized_skills.pluck(:name)
+  end
+
+  def companies
+    carrer_details.pluck(:company, :start_year, :end_year)
+  end
+
+  def company
+    carrer_details.first.company if carrer_details.any?
+  end
+
+  def designation
+    carrer_details.first.designation if carrer_details.any?
+  end
+
+  def university
+    education_details.first.university if education_details.any?
+  end
+
+  def profile_image_url
+    Rails.application.routes.url_helpers.url_for(profile_image) if profile_image.attached?
+  end
+  
+  def self.user_batches
+    self.kept.includes(:batches).map do |user|
+      { id: user.id, batch_names: user.batches.pluck(:name) }
+    end
+  end
+  
+  def self.skills_users
+    self.kept.includes(:skills).map do |user|
+      { id: user.id, skill_names: user.skills.pluck(:name) }
+    end
+  end
+  
+  def self.specialized_skills_users
+    self.kept.includes(:skills).map do |user|
+      { id: user.id, specialized_skill_names: user.specialized_skills.pluck(:name) }
+    end
+  end
+  
+  def self.user_universities
+    self.kept.includes(:education_details).map do |user|
+      { id: user.id, university_name: user.education_details.pluck(:university).first }
+    end
   end
 end
